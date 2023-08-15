3eee1436ed6f48bd585d1c5953be3417f8454f06
Hamza <hamza.afzalkhan9@gmail.com>
2023-08-15 14:15:18 +0500

    backend done basic code

--- schemas/index.js
+++ schemas/index.js
@@ -1,22 +1,14 @@
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+import user from "./user"
+import pin from "./pin"
+import comment from "./comment"
+import save from "./save"
+import postedBy from "./postedBy"
+
 
 export const schemaTypes = [
   // Document types
-  movie,
-  person,
-  screening,
+  user,pin,comment,save,postedBy,
 
   // Other types
-  blockContent,
-  plotSummary,
-  plotSummaries,
-  castMember,
-  crewMember,
+ 
 ]
