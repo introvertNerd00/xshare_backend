f9a0f5ffaa8349b300f82148ed151fc09bc49914
Hamza <hamza.afzalkhan9@gmail.com>
2023-08-15 12:52:18 +0500

    feat: bootstrap sanity studio

---
+++ sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'xshare',
+
+  projectId: 'e4r2ukwi',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
